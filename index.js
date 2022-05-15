59b781d0823f56cfc4ae362a6ae91f27399c9a0d
Patrick Young <patrickyoung281@gmail.com>
2022-05-15 14:10:04 -0400

    commit tree 37228836f9c4692ae78f510584f3a9ed211c702c

--- index.js
+++ index.js
@@ -1,1 +1,185 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+function destructivelyAppendCat () {
+    cats.push("Ralph");
+    return cats;
+};
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+    return cats;
+};
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+    return cats;
+}
+
+function appendCat () {
+    let copyCats = [...cats, "Broom"];
+    return copyCats; 
+};
+
+function prependCat () {
+    let copyCats = ["Arnold", ...cats];
+    return copyCats;
+};
+
+function removeLastCat () {
+    let copyCats = cats.slice(0, 2);
+    return copyCats;
+};
+
+
+function removeFirstCat () {
+    let copyCats = cats.slice(1);
+    return copyCats;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function destructivelyAppendCat () {
+    cats.push("Ralph");
+    return cats;
+};
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+    return cats;
+};
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+    return cats;
+};
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+    return cats;
+};
+
+function appendCat () {
+    let copyCats = [...cats, "Broom"];
+    return copyCats;
+};
+
+function prependCat () {
+    let copyCats = ["Arnold", ...cats];
+    return copyCats;
+};
+
+function removeLastCat () {
+    let copyCats = cats.slice(0, 2);
+    return copyCats;
+};
+
+function removeFirstCat () {
+    let copyCats = cats.slice(1);
+    return copyCats;
+};
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+let cats = ["Milo", "Otis", "Garfield"]; 
+
+cats.length = 0
+
+cats.push("Milo", "Otis", "Garfield"); 
+
+function destructivelyAppendCat () {
+    return cats.push("Ralph");
+};
+
+function destructivelyPrependCat () {
+    return cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat () {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    return cats.shift();
+}
+
+function appendCat () {
+    return [...cats, "Broom"];
+};
+
+function prependCat () {
+    return ["Arnold", ...cats];
+};
+
+function removeLastCat () {
+    let copyOfCats = cats.slice(0, 2);
+    return copyOfCats;
+};
+
+function removeFirstCat () {
+    let copyOfCats = cats.slice(1);
+    return copyOfCats;
+};
+
+*/
